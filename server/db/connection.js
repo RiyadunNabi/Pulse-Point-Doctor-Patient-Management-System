f5de2ae9a9e50742b8882edbfcf6d5fd18895d80
RiyadunNabi <riyadunnabi@gmail.com>
2025-08-04 14:30:57 +0600

    support both local and production DB connections

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,4 +1,5 @@
 const { Pool, types } = require("pg");
+require("dotenv").config();
 
 // 2. Define the PostgreSQL Object ID (OID) for the DATE data type.
 //    The standard OID for DATE is 1082.
@@ -9,13 +10,33 @@ const DATE_OID = 1082;
 //    do not parse it. Just return the raw string value."
 types.setTypeParser(DATE_OID, (val) => val);
 
-const pool = new Pool({
-  user: "postgres",                   // Navicat connection
-  host: "localhost",                 
-  database: "PulsePointDBProject",    // DB
-  password: "RiyadunNabi2205076",
-  port: 5432,
-});
+// const pool = new Pool({
+  //   user: "postgres",                   // Navicat connection
+  //   host: "localhost",                 
+  //   database: "PulsePointDBProject",    // DB
+  //   password: "RiyadunNabi2205076",
+  //   port: 5432,
+  // });
+  
+  let pool;
+  if (process.env.DATABASE_URL) {
+  // 🌐 Render or production environment
+  pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  });
+} else {
+  // 🖥️ Local development
+  pool = new Pool({
+    user: "postgres",
+    host: "localhost",
+    database: "PulsePointDBProject",
+    password: "RiyadunNabi2205076",
+    port: 5432,
+  });
+}
 
 module.exports = pool;
 
