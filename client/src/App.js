45f20207e222c72eb02b69563e85f573b0c64246
RiyadunNabi <riyadunnabi@gmail.com>
2025-08-04 16:16:20 +0600

    fix: add missing react-router-dom imports

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@
 
 import React, { useState, useEffect } from 'react';
 // import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
+
 import WelcomePage from './components/WelcomePage/WelcomePage';
 import AuthPage from './components/AuthPage';
 //patient
